a2cd5d86478149c939129064024265f2ee5b1f73
uk <47803499+SongCastle@users.noreply.github.com>
2021-10-17 22:44:29 +0900

    関連するモデルの情報を返却する
    Category が持つ Room の id 一覧、
    Room が持つ Category の id 一覧を返却するように修正しました。

--- back/src/app/models/category.rb
+++ back/src/app/models/category.rb
@@ -11,4 +11,11 @@ class Category < ApplicationRecord
   scope :only_sub, -> {
     joins("INNER JOIN `category_rooms` ON `category_rooms`.`category_id` = `categories`.`id` AND `category_rooms`.`type` = '#{SubCategoryRoom.to_s}'").distinct
   }
+
+  # TODO: json シリアライザを利用する
+  def to_response
+    slice(
+      :id, :name, :created_at, :creator_id
+    ).merge(room_ids: rooms.ids)
+  end
 end
